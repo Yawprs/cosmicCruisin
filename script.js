ec95d30b68ae4e48e6d283a225f6bae8476ca6aa
Drake Tyksinski <drake.tyksinski@live.com>
2020-12-23 15:54:46 -0800

    added gameLoop and Ship constructor

--- script.js
+++ script.js
@@ -1,14 +1,37 @@
 let gameBoard = document.getElementById("gameBoard")
 
-let ctx = game.getContext("2d")
+let ctx = gameBoard.getContext("2d")
 
-// Fill Color
-ctx.fillStyle = 'white';
-// Line Color
-ctx.strokeStyle = 'red';
-// Line width
-ctx.lineWidth = 5;
+gameBoard.setAttribute("height", getComputedStyle(gameBoard)["height"])
+gameBoard.setAttribute("width", getComputedStyle(gameBoard)["width"])
 
 
-ctx.fillRect(10, 10, 100, 100);
-ctx.strokeRect(10, 10, 100, 100);
+
+function Ship(x, y, color, width, height) {
+    this.x = x
+    this.y = y
+    this.color = color
+    this.width = width
+    this.height = height
+    this.render = function () {
+        ctx.fillStyle = this.color
+        ctx.fillRect (this.x, this.y, this.width, this.height)
+    }
+}
+
+let ship = new Ship(400, 460, "pink", 30, 35)
+
+
+function gameLoop () {
+    ctx.clearRect (0, 0, gameBoard.width, gameBoard.height)
+    ship.render()
+}
+
+let gameInterval = setInterval (gameLoop, 100)
+
+
+let moveShip = (e) => {
+    console.log(e.key)
+}
+
+document.addEventListener("keydown", moveShip)
